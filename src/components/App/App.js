b0ec5cf3c97e30cf590188b1433350c8a6de635f
Santiago Siri <santisiri@gmail.com>
2022-05-06 19:58:32 +0200

    polish comments

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -10,30 +10,18 @@ function App() {
 
   const myKey = ec.keyFromPrivate('4ad4dd68e2109ef52f08c7439c11c62cb60daddbcdd9cd82f197d3dcbe5fba2c');
   const myWalletAddress = myKey.getPublic('hex');
-
   let ubiChain = new Chain();
-
-  const tx1 = new Transaction(myWalletAddress, 'XXXXXXXXX', 10);
+  const tx1 = new Transaction(myWalletAddress, 'XXXXXXXXX', 7);
   tx1.signTransaction(myKey);
   ubiChain.addTransaction(tx1);
 
   console.log('Starting the miner...');
+
   ubiChain.minePendingTransactions(myWalletAddress);
 
   console.log(`Balance of my wallet is: ${ubiChain.getBalanceOfAddress(myWalletAddress)}`);
-
   console.log(`Is chain valid? ${ubiChain.isChainValid()}`);
   console.log(ubiChain.chain);
-
-
-
-
-  /* console.log('mining block 1...');
-  ubiChain.addBlock(new Block(1, new Date().toString(), { amount: 1 }));
-
-  console.log('mining block 2...');
-  ubiChain.addBlock(new Block(2, new Date().toString(), { amount: 200 }));
-  */
 
   return (
     <div className="App">
