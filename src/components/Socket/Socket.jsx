42a5c640d266e9ccb84c7615fcfc69e9b1799adb
Santiago Siri <santisiri@gmail.com>
2022-05-14 02:49:35 +0200

    building peer to peer

---
+++ src/components/Socket/Socket.jsx
@@ -0,0 +1,31 @@
+import Peer from 'peerjs';
+
+/**
+* @summary Creates a block of transactions.
+*/
+export default class Socket {
+  constructor(id, relayServer) {
+    this.id = id;
+    this.relayServer = relayServer;
+    this.peer = new Peer();
+    this.peerList = [];
+  }
+
+  // Connects to a relay server to obtain the address of other peers.
+  getPeers() {
+    this.peer = new Peer(this.id, {
+      host: this.relayServer,
+      port: 9001,
+      path: '/myapp'
+    });
+
+    console.log(this.peer);
+  // console.log(this.peer.listAllPeers());
+    // console.log(this.peer.listAllPeers());
+  }
+
+  // Compose message to be transmitted to a peer
+  produceMessage(type, data) {
+    return { type, data };
+  }
+}
