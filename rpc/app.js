5c4f54b00bea2fba4eb4f36c813393a650cf0a72
Santiago Siri <santisiri@gmail.com>
2022-05-23 18:27:04 +0200

    rpc sub repo

---
+++ rpc/app.js
@@ -0,0 +1,64 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+const bodyParser = require("body-parser");
+const { JSONRPCServer } = require("json-rpc-2.0");
+const server = new JSONRPCServer();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+app.post("/json-rpc", (req, res) => {
+  const jsonRPCRequest = req.body;
+  console.log(req.body);
+  // server.receive takes a JSON-RPC request and returns a promise of a JSON-RPC response.
+  // It can also receive an array of requests, in which case it may return an array of responses.
+  // Alternatively, you can use server.receiveJSON, which takes JSON string as is (in this case req.body).
+  server.receive(jsonRPCRequest).then((jsonRPCResponse) => {
+    if (jsonRPCResponse) {
+      res.json(jsonRPCResponse);
+    } else {
+      // If response is absent, it was a JSON-RPC notification method.
+      // Respond with no content status (204).
+      res.sendStatus(204);
+    }
+  });
+});
+
+app.listen(1337);
+
+module.exports = app;
