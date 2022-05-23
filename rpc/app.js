48045e3b1f482aa7c3a5c276fe13de05af5148b9
Santiago Siri <santisiri@gmail.com>
2022-05-23 18:57:45 +0200

    spawns json rpc server at root

--- rpc/app.js
+++ rpc/app.js
@@ -1,48 +1,22 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
-
+const express = require('express')
 const bodyParser = require("body-parser");
 const { JSONRPCServer } = require("json-rpc-2.0");
+
 const server = new JSONRPCServer();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+console.log('Starting JSON-RPC server...')
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// First parameter is a method name.
+// Second parameter is a method itself.
+// A method takes JSON-RPC params and returns a result.
+// It can also return a promise of the result.
+server.addMethod("echo", ({ text }) => text);
+server.addMethod("log", ({ message }) => console.log(message));
+
+const app = express();
 app.use(bodyParser.json());
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-app.post("/json-rpc", (req, res) => {
+app.post("/", (req, res) => {
   const jsonRPCRequest = req.body;
   console.log(req.body);
   // server.receive takes a JSON-RPC request and returns a promise of a JSON-RPC response.
@@ -59,6 +33,4 @@ app.post("/json-rpc", (req, res) => {
   });
 });
 
-app.listen(1337);
-
-module.exports = app;
+app.listen(1024);
