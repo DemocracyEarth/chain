040ea7d893fd0beed50c6f6da4f0a106a7770753
Santiago Siri <santisiri@gmail.com>
2022-05-25 19:18:39 +0200

    downgraded to react v17

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
@@ -11,11 +11,11 @@ server.addMethod("echo", ({ text }) => text);
 server.addMethod("log", ({ message }) => console.log(message));
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
