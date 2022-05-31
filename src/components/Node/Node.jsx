c4e8ca9ad5c3ffb820b31bf931d7981df6a24283
Santiago Siri <santisiri@gmail.com>
2022-06-01 01:03:24 +0200

    polished ux

--- src/components/Node/Node.jsx
+++ src/components/Node/Node.jsx
@@ -15,31 +15,16 @@ export default class Node extends Component {
     this.relayServer = config.relay.server;
     this.peer = new Peer();
     this.peerList = [];
-    
+    this.provider = new ethers.providers.InfuraProvider("mainnet", config.provider.walletconnect.options.infuraId);
+
     this.state = {
       isHuman: false,
       verified: false,
       peers: []
     }
-
-    // Connect to the network
-    this.provider = new ethers.providers.InfuraProvider("mainnet", config.provider.walletconnect.options.infuraId);
-
-    // The address from the above deployment example
-    let contractAddress = config.contract.proofofhumanity;
-
-    // We connect to the Contract using a Provider, so we will only
-    // have read-only access to the Contract
-    this.contract = new ethers.Contract(contractAddress, abi, this.provider);
   }
 
   async componentDidMount() {
-    await this.isHuman();
-  }
-
-  async isHuman() {
-    // Get the current value
-    this.setState({ isHuman: await this.contract.isRegistered(this.props.address), verified: true });
     this.setState({ peers: await this.getPeers() });
   }
 
@@ -58,22 +43,11 @@ export default class Node extends Component {
   render() {
     return (
       <>
-        {(this.state.isHuman) ?
-          <p>Human Node — Can Validate</p>
-          :
-          <p>
-            {(this.state.verified) ?
-              'Not Human'
-              :
-              '...'
-            }
-          </p>
-        }
         <Account address={this.props.address} provider={this.provider} />
-        <p>
+        {/** <p>
           Connected Peers:
           {JSON.stringify(this.state.peers)}
-        </p>
+        </p> **/}
       </>
     )
   }
